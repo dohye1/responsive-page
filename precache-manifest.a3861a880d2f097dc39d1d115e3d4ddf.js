0e78f8e5c351b1e6a13a5579ada2da253c511f2c
hyewon do <tsc03018@gmail.com>
2020-10-02 19:51:46 +0900

    Updates

---
+++ precache-manifest.a3861a880d2f097dc39d1d115e3d4ddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c81e55e32a5f6c5d1927c54e707af0f",
+    "url": "/responsive-page/index.html"
+  },
+  {
+    "revision": "d0da9582159464c32da1",
+    "url": "/responsive-page/static/css/main.d1524c9d.chunk.css"
+  },
+  {
+    "revision": "a590fd8359407e50efe9",
+    "url": "/responsive-page/static/js/2.d349d35c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/responsive-page/static/js/2.d349d35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0da9582159464c32da1",
+    "url": "/responsive-page/static/js/main.d024a32c.chunk.js"
+  },
+  {
+    "revision": "1be053b85ef36e66be0c",
+    "url": "/responsive-page/static/js/runtime-main.8b725617.js"
+  }
+]);
